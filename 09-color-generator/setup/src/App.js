bf1e0c0936648a68a0ec00c3e1f046867a59c0b3
Sergio Barria <sergio.barria@icloud.com>
2021-01-27 17:03:35 -0500

    finishe projects 08 to 10

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,54 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
 
-import Values from 'values.js'
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#00ff00').all(10));
+
+  const handlerSubmit = e => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+  return (
+    <React.Fragment>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handlerSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={e => setColor(e.target.value)}
+            placeholder='#00ff00'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,35 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
-export default List
+const List = ({ items, removeItem, editItem }) => {
+  return (
+    <div className='grocery-list'>
+      {items.map(item => {
+        const { id, title } = item;
+        return (
+          <article key={id} className='grocery-item'>
+            <p className='title'>{title}</p>
+            <div className='btn-container'>
+              <button
+                className='edit-btn'
+                type='button'
+                onClick={() => editItem(id)}
+              >
+                <FaEdit />
+              </button>
+              <button
+                className='delete-btn'
+                type='button'
+                onClick={() => removeItem(id)}
+              >
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
