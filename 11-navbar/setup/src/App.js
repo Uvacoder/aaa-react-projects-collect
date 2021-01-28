b36b6dd4bf398461e1d202830257c482bd217284
Sergio Barria <sergio.barria@icloud.com>
2021-01-27 22:47:13 -0500

    finished project 11

--- 11-navbar/setup/src/App.js
+++ 11-navbar/setup/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
 function App() {
   return (
-    <>
-      <h2>navbar project setup</h2>
-    </>
-  )
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
