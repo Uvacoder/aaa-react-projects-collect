bbfca528f7801e625e9aedaadd5ae09a34d7f79a
Sergio Barria <sergio.barria@icloud.com>
2021-01-28 23:21:04 -0500

    finished project 14

--- 14-cart/setup/src/CartContainer.js
+++ 14-cart/setup/src/CartContainer.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import CartItem from './CartItem'
-import { useGlobalContext } from './context'
+import React from 'react';
+import CartItem from './CartItem';
+import { useGlobalContext } from './context';
 
 const CartContainer = () => {
-  const { cart } = useGlobalContext()
+  const { cart, total, clearCart } = useGlobalContext();
   if (cart.length === 0) {
     return (
       <section className='cart'>
@@ -13,8 +13,9 @@ const CartContainer = () => {
           <h4 className='empty-cart'>is currently empty</h4>
         </header>
       </section>
-    )
+    );
   }
+
   return (
     <section className='cart'>
       {/* cart header */}
@@ -23,8 +24,8 @@ const CartContainer = () => {
       </header>
       {/* cart items */}
       <div>
-        {cart.map((item) => {
-          return <CartItem key={item.id} {...item} />
+        {cart.map(item => {
+          return <CartItem key={item.id} {...item} />;
         })}
       </div>
       {/* cart footer */}
@@ -32,18 +33,15 @@ const CartContainer = () => {
         <hr />
         <div className='cart-total'>
           <h4>
-            total <span>$0.00</span>
+            total <span>${total}</span>
           </h4>
         </div>
-        <button
-          className='btn clear-btn'
-          onClick={() => console.log('clear cart')}
-        >
+        <button className='btn clear-btn' onClick={clearCart}>
           clear cart
         </button>
       </footer>
     </section>
-  )
-}
+  );
+};
 
-export default CartContainer
+export default CartContainer;
