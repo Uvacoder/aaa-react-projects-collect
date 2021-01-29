962405ffbe27d23b58e222aa6861cd55fd8bfb05
Sergio Barria <sergio.barria@icloud.com>
2021-01-28 20:56:55 -0500

    finished project 12 to 13

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,8 +1,20 @@
-import React, { useContext } from 'react'
-import { FaBars } from 'react-icons/fa'
+import React, { useContext } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { AppContext, useGlobalContext } from './context';
 
 const Home = () => {
-  return <h2>home component</h2>
-}
+  const { openSidebar, openModal } = useGlobalContext();
 
-export default Home
+  return (
+    <main>
+      <button className='sidebar-toggle' onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className='btn' onClick={openModal}>
+        Show Modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
