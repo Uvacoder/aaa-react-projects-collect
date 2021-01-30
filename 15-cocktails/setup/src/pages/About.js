e83a1d9b0ba19e275f7f613d5ee19f890007e11f
Sergio Barria <sergio.barria@icloud.com>
2021-01-29 20:13:12 -0500

    finished project 15

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        exercitationem natus officiis a mollitia odit autem, fugit repellat
+        tempore impedit! Quisquam magni eius optio voluptatem nihil, tempore
+        sint. Veniam ipsa odio veritatis, nulla sed enim illo quisquam labore
+        atque obcaecati.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
